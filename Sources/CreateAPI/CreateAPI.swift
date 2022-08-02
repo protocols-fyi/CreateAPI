a46872bbc227d4e267e72a484b1dedb7e60333d0
Liam Nichols <liam.nichols.ln@gmail.com>
2022-08-02 17:11:04 +0200

    Define version number for cli tool (#76)

--- Sources/CreateAPI/CreateAPI.swift
+++ Sources/CreateAPI/CreateAPI.swift
@@ -9,6 +9,7 @@ struct CreateAPI: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "create-api",
         abstract: "A Swift command-line tool to auto-generate code for OpenAPI specs",
+        version: "0.0.5",
         subcommands: [Generate.self]
     )
 }
